78edffb155a8562b7c0d5d6a4bc31e084fa32183
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-10-30 16:58:37 +0000

    AV

--- VideoPlayer.m
+++ VideoPlayer.m
@@ -123,6 +123,11 @@
 											 selector:@selector(movieFinishedCallback:)  
 												 name:MPMoviePlayerPlaybackDidFinishNotification  
 											   object:[moviePlayerViewController moviePlayer]];
+        NSError *_error = nil;
+        
+        [[AVAudioSession sharedInstance] setCategory: AVAudioSessionCategoryPlayback error:&_error];
+        
+
     
     [self presentMoviePlayerViewControllerAnimated:moviePlayerViewController];
     
